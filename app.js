bf520ebfa4524ce59a12ae4da09aa6e5b09a1971
James Reid <jreidmke@gmail.com>
2021-04-19 11:03:42 -0500

    find boundary 1

--- app.js
+++ app.js
@@ -15,3 +15,21 @@ function binarySearch(arr, target) {
     };
     return -1;
 }
+
+
+function find_boundary(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+    let idx = -1
+    while(left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if(arr[mid]) {
+            idx = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;   
+        };
+
+    };
+    return idx;
+}
