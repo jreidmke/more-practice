d64a4094f75d040ba0f26e69346b1ab69f3f0099
James Reid <jreidmke@gmail.com>
2021-04-19 11:07:35 -0500

    find boundary 3

--- app.js
+++ app.js
@@ -49,3 +49,21 @@ function firstNotSmaller(arr, target) {
     };
     return idx;
 }
+
+function findFirstOccurrence(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+    let idx = -1
+    while(left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if(arr[mid] === target) {
+            idx = mid;
+            right = mid - 1
+        } else if(arr[mid] > target) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;    
+        };
+    };
+    return idx;
+}
