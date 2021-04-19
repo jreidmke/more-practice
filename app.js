551d03f2074f7339621ed0e750f870ff01941cae
James Reid <jreidmke@gmail.com>
2021-04-19 11:17:04 -0500

    find boundary 4

--- app.js
+++ app.js
@@ -83,3 +83,19 @@ function squareRoot(n) {
     };
     return sq;
 }
+
+function findMinRotated(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+    let idx;
+    while(left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if(arr[mid] <= arr[arr.length - 1]) {
+            idx = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;    
+        };
+    };
+    return idx;
+}
