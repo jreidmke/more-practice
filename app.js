4aa10a697aaa33cd3cb22efd9954cae48ff74d69
James Reid <jreidmke@gmail.com>
2021-04-19 11:18:21 -0500

    find boundary 5

--- app.js
+++ app.js
@@ -99,3 +99,20 @@ function findMinRotated(arr) {
     };
     return idx;
 }
+
+function peakOfMountainArray(arr) {
+    let left = 0;
+    let right = arr.length - 1;
+    let idx;
+    while(left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if(arr[mid] > arr[mid + 1]) {
+            idx = mid;
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        };
+
+    };
+    return idx;
+}
