eb785ad05d83111e2e029c39459bbf13564b21b5
James Reid <jreidmke@gmail.com>
2021-04-23 12:03:42 -0500

    needle in haystack

--- app.js
+++ app.js
@@ -677,3 +677,14 @@ function reverseEachWord(arr, newArr=[]) {
     newArr.push(str);
     return reverseEachWord(arr.slice(1), newArr);
 };
+
+function needleInHaystack(haystack, needle) {
+    let count = 0;
+    for(let i = 0; i < haystack.length; i++) {
+        for(let j = 0; j < needle.length; j++) {
+            if(haystack[i + j] !== needle[j]) break;
+            if(j === needle.length - 1) count++;
+        };
+    };
+    return count;
+};
