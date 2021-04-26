3260e9a59b542ba9afe46b818e7c69238c758f0b
James Reid <jreidmke@gmail.com>
2021-04-26 10:06:43 -0500

    new is valid bst

--- app.js
+++ app.js
@@ -837,3 +837,12 @@ function maxProduct(nums) {
     helper(nums, [1]);
     return longest;
 }
+
+var isValidBST = function(root, min = -Infinity, max = Infinity) {
+    if(root === null)
+        return true;
+    if(root.val <= min || root.val >= max)
+        return false;
+    return isValidBST(root.right, root.val, max) && isValidBST(root.left, min, root.val)
+};
+
