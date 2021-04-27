f9f78b4b5a1a53844ab760d9629d797932a89c54
James Reid <jreidmke@gmail.com>
2021-04-27 13:10:35 -0500

    swap zeroes

--- app.js
+++ app.js
@@ -862,4 +862,15 @@ var longestPalindrome = function(s) {
       }
     }
     return max;
-  };
+};
+
+function swapZeroes(nums) {
+    let slow = 0;
+    for(let fast = 0; fast < nums.length; fast++) {
+        if(nums[fast]) {
+            [nums[slow], nums[fast]] = [nums[fast], nums[slow]];
+            slow++;
+        }
+    }
+    return nums;
+} 
