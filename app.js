617300288b622fd54c73078e00583d36bf8829a4
James Reid <jreidmke@gmail.com>
2021-04-28 11:28:15 -0500

    new method of serialize/deseriealize

--- app.js
+++ app.js
@@ -874,3 +874,37 @@ function swapZeroes(nums) {
     }
     return nums;
 } 
+
+function serialize(root) {
+    let data = [];
+  
+    function go(node) {
+      if (node == null) {
+        data.push(null);
+        return;
+      }
+  
+      data.push(node.val);
+      go(node.left);
+      go(node.right);
+    }
+  
+    go(root);
+    return data;
+  }
+  
+  function deserialize(data) {
+    function go() {
+      if (data.length === 0) return;
+  
+      const val = data.shift();
+      if (val == null) return null;
+  
+      const node = new TreeNode(val);
+      node.left = go();
+      node.right = go();
+      return node;
+    }
+  
+    return go();
+  }
