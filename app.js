628f3608f6b66c23c69473ece27d34bdcb1281a4
James Reid <jreidmke@gmail.com>
2021-04-28 15:15:50 -0500

    new reverse string method

--- app.js
+++ app.js
@@ -929,3 +929,15 @@ function serialize(root) {
     }
     return max;
 };
+
+var reverseString = function(s) {
+    let left = 0;
+    let right = s.length - 1;
+    while(left <= right) {
+      [s[left], s[right]] = [s[right], s[left]]
+      left++;
+      right--;
+    }
+    return s
+  };
+  
