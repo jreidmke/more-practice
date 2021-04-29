d36860186f40c124554a379dd4be7d98d43f3f3d
James Reid <jreidmke@gmail.com>
2021-04-29 12:03:45 -0500

    smaller number than current

--- app.js
+++ app.js
@@ -1072,8 +1072,19 @@ function exist(board, word) {
     let count = 0;
     for(let n of nums) {
       !map[n] ? map[n] = 1 : count += map[n]++
-      console.log(map)
-      console.log(count)
     };
     return count;
   }
+
+  function smallerNumbersThanCurrent(nums) {
+    let arr = [];
+  
+    for(let i = 0; i < nums.length; i++) {
+      let count = 0;
+      for(let j = 0; j < nums.length; j++) {
+        if(nums[i] > nums[j]) count++;
+      }
+      arr.push(count);
+    };
+    return arr;
+  };
