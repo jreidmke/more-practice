33d9a9558ecc3852dac3a1ad08f1c83f527dddd3
James Reid <jreidmke@gmail.com>
2021-04-29 11:09:00 -0500

    stones in jewels

--- app.js
+++ app.js
@@ -1046,3 +1046,11 @@ function exist(board, word) {
     };
     return nums;
   }
+
+  var numJewelsInStones = function(jewels, stones) {
+    let count = 0;
+    for(let i = 0; i < stones.length; i++) {
+      if(jewels.indexOf(stones[i]) !== -1) count++;
+    };
+    return count;
+  };
