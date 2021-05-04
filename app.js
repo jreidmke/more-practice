7c7149fe2e49e187b772945a15ac3eeefcea4c70
James Reid <jreidmke@gmail.com>
2021-05-04 11:18:53 -0500

    is toeplitz matrix

--- app.js
+++ app.js
@@ -1541,3 +1541,12 @@ function createTargetArray(nums, idx) {
     }
     return target;
 }
+
+function isToeplitzMatrix(matrix) {
+    for(let i = 0; i < matrix.length - 1; i++) {
+        for(let j = 0; j < matrix[0].length - 1; j++) {
+            if(matrix[i + 1][j + 1] !== matrix[i][j]) return false;
+        }
+    };
+    return true;
+}
