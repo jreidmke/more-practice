526d5e4713ae0358ed5d6cdb0a4e0f329aaa6578
James Reid <jreidmke@gmail.com>
2021-05-10 12:10:11 -0500

    sort colors

--- app.js
+++ app.js
@@ -1803,3 +1803,27 @@ var fourSumCount = function(A, B, C, D) {
     }
     return output;
 };
+
+function sortColors(nums) {
+    function swap(i, j) {
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+  
+    let l = 0;
+    let r = nums.length - 1;
+    let i = 0;
+  
+    while (i <= r) {
+      const n = nums[i];
+      if (n === 0) {
+        swap(i, l);
+        l++;
+        i++;
+      } else if (n === 2) {
+        swap(i, r);
+        r--;
+      } else {
+        i++;
+      }
+    }
+  }
