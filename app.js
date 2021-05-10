aeabc27cd6330d4fe4baaffa1c74f70bd9393b42
James Reid <jreidmke@gmail.com>
2021-05-10 12:07:56 -0500

    4 sum count

--- app.js
+++ app.js
@@ -1784,3 +1784,22 @@ var rotate = function(M) {
         }
     }
 };
+
+var fourSumCount = function(A, B, C, D) {
+    if (!A || !B || !C || !D) return [];
+    // cuadratic solution... counter, reduced from sum_of_two
+    let memory = new Map();
+    for (let i of A) {
+        for (let j of B) {
+            memory.set(-(i + j), memory.get(-(i + j)) + 1 || 1);
+        }
+    }
+    // look in the map if we have seen the complement of this sum
+    let output = 0;
+    for (let m of C) {
+        for (let n of D) {
+            output += memory.has(m + n)? memory.get(m + n): 0;
+        }
+    }
+    return output;
+};
