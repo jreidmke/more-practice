c524200d32cf1ef8975af4154a09a68b1c479238
James Reid <jreidmke@gmail.com>
2021-05-13 11:39:07 -0500

    num pairs divisible by 60

--- app.js
+++ app.js
@@ -2100,3 +2100,15 @@ function isPrime(n) {
     };
     return true;
 }
+
+const numPairsDivisibleBy60 = (time) => {
+    const appearDic = {};
+    let ans = 0;
+    time.forEach(el => {
+        const mod = el % 60;
+        const left = (60 - mod) % 60;
+        ans += appearDic[left] ? appearDic[left] : 0;
+        appearDic[mod] = appearDic[mod] ? appearDic[mod] + 1 : 1;
+    });
+    return ans;
+};
