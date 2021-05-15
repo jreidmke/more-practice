de2c0ff39f6d1f9f1b963193435ffbc9b8d5e2fc
James Reid <jreidmke@gmail.com>
2021-05-15 13:00:18 -0500

    lowest common ancestor for unsorted tree

--- app.js
+++ app.js
@@ -2156,3 +2156,10 @@ function numSquares(n) {
     }
     return dp[n];
 }
+
+function lowestCommonAncestor(root, p, q) {
+    if(!root || root === p || root === q) return root;
+    let resL = lowestCommonAncestor(root.left, p, q);
+    let resR = lowestCommonAncestor(root.right, p, q);
+    return (resL && resR) ? root : (resL || resR);
+}
