e4189088d70c8e061c5d5f9de8015d3e435b0fd5
James Reid <jreidmke@gmail.com>
2021-05-15 12:57:20 -0500

    four sum count

--- app.js
+++ app.js
@@ -2125,3 +2125,22 @@ var topKFrequent = function(words, k) {
     })
     return arr.slice(0, k);
 };
+
+var fourSumCount = function(A, B, C, D) {
+    if (!A || !B || !C || !D) return [];
+    // cuadratic solution... counter, reduced from sum_of_two
+    let memory = new Map();
+    for (let i of A) {
+        for (let j of B) {
+            memory.set(-(i + j), memory.get(-(i + j)) + 1 || 1);
+        }
+    }
+    // look in the map if we have seen the complement of this sum
+    let output = 0;
+    for (let m of C) {
+        for (let n of D) {
+            output += memory.has(m + n)? memory.get(m + n): 0;
+        }
+    }
+    return output;
+};
