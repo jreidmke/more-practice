8744d59c33f0252b3c2407b2e89de607efb715c4
James Reid <jreidmke@gmail.com>
2021-05-28 13:04:04 -0500

    min path sum

--- app.js
+++ app.js
@@ -2611,3 +2611,23 @@ function findMedianSortedArrays(nums1, nums2) {
     }
     return big;
 }
+
+var minPathSum = function(grid) {
+    const i = grid.length - 1
+    const j = grid[0].length - 1
+    
+    for(let k = 0; k <= i; ++k) {
+        for(let l = 0; l <= j; ++l) {
+            if(k > 0 && l > 0)
+                grid[k][l] = Math.min(grid[k][l] + grid[k][l - 1], grid[k - 1][l] + grid[k][l])
+            else if(k > 0 || l > 0) {
+                if(l > 0)
+                    grid[k][l] += grid[k][l - 1]
+                else
+                    grid[k][l] += grid[k - 1][l]
+                }   
+        }
+    }
+    
+    return grid[i][j]
+};
